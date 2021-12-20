a8cab5e21646cec48ae185a8760f1b906b784885
fereshteh-dehghani <fdehghani666@gmail.com>
2021-12-20 21:33:02 +0330

    Exercise

---
+++ jsExercise.js
@@ -0,0 +1,80 @@
+// let number=14526987;
+
+
+// function digiOfNumber(number){
+//     //convert number to string
+//     let convertString=number.toString();
+//     //get length of string
+//     let getLength=convertString.length;
+//     return getLength;
+// }
+
+// console.log(digiOfNumber(number))
+
+// let arr=[1,2,3];
+// let arr1=[1,2,3,4,6,7];
+
+// function getLengthOfArray(arrey){
+//   let lengthArray=arrey.length;
+//   if(lengthArray<4){
+//       console.log('Length is less than 4');
+//   }  else{
+//       console.log(`length is ${lengthArray}`);
+//   }
+  
+// }
+// getLengthOfArray(arr);
+// getLengthOfArray(arr1);
+
+// function bigger(num1,num2,num3){
+//     let bigNumber=num1;
+//     if(bigNumber<num2){
+//         bigNumber=num2;
+
+//     }if(bigNumber<num3){
+//         bigNumber=num3;
+//     }
+//     console.log(`big Number is : ${bigNumber}`)
+// }
+// function MaxNumber(num1,num2,num3) {
+//     if (num1 > num2) {
+//       console.log(num1);
+//     } else if(num2>num3) {
+//       console.log(num2);
+//     }
+//     else {
+//       console.log(num3);
+//     }
+//   }
+//   MaxNumber(78,14,8)
+
+// let num1=1458967;
+// let num2=458;
+// let num3=15986;
+// MaxNumber(num1,num2,num3);
+
+// let array1=["apple","banana","pineApple","orange","Lemon","mango"];
+
+// function getLastElement(array){
+//     let lastIndexArrey=
+// }
+
+let arr1=["zahra amini","classA","aps",[10,20,19,18,17]];
+
+let arr2=["zahra amini","classA","aps",[10,20,19,20,17]];
+
+
+function getAverage(array){
+    // get grades
+let grades=array [array.length-1];
+let sumGrades=(grades[0] +grades[1] +grades[2]+grades[3]+grades[4]);
+let  average=sumGrades/grades.length;
+array.push(average);
+
+
+return array;
+
+
+}
+getAverage(arr2)
+console.log(arr2);
